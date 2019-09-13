914c98f11133837cdbdf96dff920662ab8b13629
Vishal Kumar <vr16796@gmail.com>
2019-09-13 20:42:56 +0530

    day1

---
+++ lifeUniverse&Everything.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main() {
+    int t ;
+    while(scanf("%d",&t) != EOF) {
+        if(t == 42)
+        {
+            break;
+        }
+        cout << t << endl;
+    }
+    return 0 ;
+}
