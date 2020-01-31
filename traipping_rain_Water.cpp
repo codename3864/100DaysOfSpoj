00e748fa4a460c0c38b2bd4dd8b826b3f39aa200
Vishal Rajak <vkr993216@gmail.com>
2020-02-01 04:18:23 +0530

    trapping water

--- traipping_rain_Water.cpp
+++ traipping_rain_Water.cpp
@@ -1,3 +1,30 @@
+/*
+
+Given an array arr[] of N non-negative integers representing height of blocks at index i as Ai where
+the width of each block is 1. Compute how much water can be trapped in between blocks after raining.
+
+Constraints:
+1 <= T <= 100
+3 <= N <= 107
+0 <= Ai <= 108
+
+Example:
+Input:
+2
+4
+7 4 0 9
+3
+6 9 9
+
+Output:
+10
+0
+
+Explanation:
+Testcase 1: Water trapped by block of height 4 is 3 units, block of height 0 is 7 units.
+So, total unit of water trapped is 10 units.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
